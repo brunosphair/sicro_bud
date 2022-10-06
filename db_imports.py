4691432194f5ab3e85a8d81ded798814edd91867
sphair <brunosphair@gmail.com>
2022-10-06 01:41:35 -0300

    general update

---
+++ db_imports.py
@@ -0,0 +1,246 @@
+import openpyxl
+import tkinter as tk
+from tkinter import filedialog
+import json
+
+def get_xlsx_materials(filepath=None):
+    if not filepath:
+        root = tk.Tk()
+        root.withdraw()
+        filepath = filedialog.askopenfilename(title="Selecione o arquivo referente ao Relatório Sintético de Materiais",
+                                                filetypes=[("Excel files", "*.xlsx")]) 
+    ps = openpyxl.load_workbook(filepath)
+    sheet = ps.active
+
+    table_rows = []
+    row = 1
+
+    while sheet['A' + str(row)].value != "Código":
+        row += 1
+
+    row += 1
+
+    while sheet['A' + str(row)].value is None:
+        row += 1
+
+    while row <= sheet.max_row:
+        materials = {}
+        materials["code"] = sheet['A' + str(row)].value
+        materials["description"] = sheet['B' + str(row)].value
+        materials["unit"] = sheet['C' + str(row)].value
+        if sheet['D' + str(row)].value == '-':
+            materials["price"] = 0
+        else:
+            materials["price"] = sheet['D' + str(row)].value
+        table_rows.append(materials)
+        row = row + 1
+
+    ps.close()
+
+    return table_rows
+
+def get_xlsx_equipments(filepath=None):
+    '''
+    Imports the equipments data from the *.xlsx file to a list of dictionaries.
+    '''
+
+    if not filepath:
+        root = tk.Tk()
+        root.withdraw()
+        filepath = filedialog.askopenfilename(title='Selecione o arquivo referente ao Relatório Sintético de Equipamentos',
+                                            filetypes=[("Excel files", "*.xlsx")])
+
+    ps = openpyxl.load_workbook(filepath)
+    sheet = ps.active
+
+    table_rows = []
+    row = 1
+
+    while sheet['A' + str(row)].value != "Código":
+        row += 1
+    row += 1
+
+    while sheet['A' + str(row)].value is None:
+        row += 1
+
+    while row <= sheet.max_row:
+        equips = {}
+        equips["code"] = sheet['A' + str(row)].value
+        equips["description"] = sheet['B' + str(row)].value
+        equips["acquisition_price"] = sheet['C' + str(row)].value
+        equips["depreciation"] = sheet['D' + str(row)].value
+        equips["capital_opportunity"] = sheet['E' + str(row)].value
+        equips["insurance_and_taxes"] = sheet['F' + str(row)].value
+        equips["maintenance"] = sheet['G' + str(row)].value
+        equips["operation"] = sheet['H' + str(row)].value
+        equips["operation_labor"] = sheet['I' + str(row)].value
+        equips["productive_cost"] = sheet['J' + str(row)].value
+        equips["unproductive_cost"] = sheet['K' + str(row)].value
+        table_rows.append(equips)
+        row = row + 1
+
+    ps.close()
+
+    return table_rows
+
+def get_xlsx_labors(filepath=None):
+    '''
+    Imports the labors data from the *.xlsx file to a dictionary.
+    '''
+
+    if not filepath:
+        root = tk.Tk()
+        root.withdraw()
+        filepath = filedialog.askopenfilename(title='Selecione o arquivo referente ao Relatório Sintético de Mão de Obra',
+                                            filetypes=[("Excel files", "*.xlsx")])
+
+    ps = openpyxl.load_workbook(filepath)
+    sheet = ps.active
+
+    table_rows = []
+    row = 1
+
+    while sheet['A' + str(row)].value != "Código":
+        row += 1
+    row += 1
+
+    while sheet['A' + str(row)].value is None:
+        row += 1
+
+    while row <= sheet.max_row:
+        labor = {}
+        labor["code"] = sheet['A' + str(row)].value
+        labor["description"] = sheet['B' + str(row)].value
+        labor["unit"] = sheet['C' + str(row)].value
+        labor["salary"] = sheet['D' + str(row)].value
+        labor["total_charges"] = sheet['E' + str(row)].value
+        labor["cost"] = sheet['F' + str(row)].value
+        labor["dangerousness_and_insalubrity"] = sheet['G' + str(row)].value
+        table_rows.append(labor)
+        row += 1
+
+    ps.close()
+
+    return table_rows
+
+def get_json_gd_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_general_data = []
+    for key in dict_import:
+        general_data={}
+        general_data["comp_code"] = key
+        general_data["description"] = dict_import[key][0]
+        general_data["productivity_unit"] = dict_import[key][1][1]
+        general_data["productivity"] = dict_import[key][1][0]
+        general_data["fic"] = int(dict_import[key][8])
+        sicro_general_data.append(general_data)
+        
+        # comps[key] = Composition(dict_import[key][0], dict_import[key][1], dict_import[key][2], dict_import[key][3],
+        #                         dict_import[key][4], dict_import[key][5],
+        #                         dict_import[key][6], dict_import[key][7], dict_import[key][8])
+    
+    return sicro_general_data
+    
+
+# comps[key] = Composition(dict_import[key][6], dict_import[key][7])
+
+def get_json_equipments_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_equipments = []
+    for key in dict_import:
+        for item in dict_import[key][2]:
+            equipment = {}
+            equipment["comp_code"] = key
+            equipment["equipment_code"] = item[0]
+            equipment["equipment_quantity"] = item[1]
+            equipment["equipment_operative"] = item[2]
+            equipment["equipment_inoperative"] = item[3]
+            sicro_equipments.append(equipment)
+    
+    return sicro_equipments
+
+def get_json_labors_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_labors = []
+    for key in dict_import:
+        for item in dict_import[key][3]:
+            labor = {}
+            labor["comp_code"] = key
+            labor["labor_code"] = item[0]
+            labor["labor_quantity"] = item[1]
+            sicro_labors.append(labor)
+    
+    return sicro_labors
+
+def get_json_materials_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_materials = []
+    for key in dict_import:
+        for item in dict_import[key][4]:
+            material = {}
+            material["comp_code"] = key
+            material["material_code"] = item[0]
+            material["material_quantity"] = item[1]
+            sicro_materials.append(material)
+    
+    return sicro_materials
+
+def get_json_auxiliary_activities_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_aux = []
+    for key in dict_import:
+        for item in dict_import[key][5]:
+            aux = {}
+            aux["comp_code"] = key
+            aux["auxiliary_activity_code"] = item[0]
+            aux["auxiliary_activity_quantity"] = item[1]
+            sicro_aux.append(aux)
+    
+    return sicro_aux
+
+def get_json_fixed_time_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_fixed_time = []
+    for key in dict_import:
+        for item in dict_import[key][6]:
+            fixed_time = {}
+            fixed_time["comp_code"] = key
+            fixed_time["item_code"] = item[0]
+            fixed_time["fixed_time_code"] = item[1]
+            fixed_time["fixed_time_quantity"] = item[2]
+            sicro_fixed_time.append(fixed_time)
+    
+    return sicro_fixed_time
+
+def get_json_transportation_sicro(filename):
+
+    with open(filename, 'r') as fp:
+        dict_import = json.load(fp)
+    sicro_transportation = []
+    for key in dict_import:
+        for item in dict_import[key][7]:
+            transportation = {}
+            transportation["comp_code"] = key
+            transportation["item_code"] = item[0]
+            transportation["transportation_quantity"] = item[1]
+            transportation["transportation_code_ln"] = item[2]
+            transportation["transportation_code_rp"] = item[3]
+            transportation["transportation_code_p"] = item[4]
+            sicro_transportation.append(transportation)
+    
+    return sicro_transportation
+
+if __name__=='__main__':
+    print(get_json_equipments_sicro('comps.json'))
--- sicro.py
+++ sicro.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
-import json
 import openpyxl
 import db_mod
-import decimal
+import json
 
 
 class Composition:
@@ -34,147 +33,149 @@ class Composition:
     def __str__(self):
         return self.name
 
-    def comp_sum(self, state, date, Tables):
-        '''
-        Do the sum of all the costs inherent to a composition: equipments, 
-        labor, materials, auxiliary activities, fixed time and transportation.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
+def comp_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Do the sum of all the costs inherent to a composition: equipments, 
+    labor, materials, auxiliary activities, fixed time and transportation.
+    '''
+    if not hasattr(Tables, 'comps'):
+        Tables.json_import()
 
-        fic_value = self.fic
-        unit_cost = (self.equips_sum(Tables) + self.labor_sum(Tables)) / self.prod[0]
-        sum = round(unit_cost + unit_cost * fic_value + self.materials_sum(state, date, Tables) \
-            + self.aux_activity_sum(state, date, Tables) + self.fixed_time_sum(state, date, Tables), 2)
-        
-        # + SomaTransporte(CompCode)
-        # TODO: Find where to put the transport_sum
-
-        return sum
+    fic_value = Sicro.general_data.get_fic(comp_code)
+    unit_cost = (equips_sum(comp_code, state, date, Tables, Sicro) + labor_sum(comp_code, state, date, Tables, Sicro)) / Sicro.general_data.get_productivity(comp_code)
+    sum = round(unit_cost + unit_cost * fic_value + materials_sum(comp_code, state, date, Tables, Sicro) \
+        + aux_activity_sum(comp_code, state, date, Tables, Sicro) + fixed_time_sum(comp_code, state, date, Tables, Sicro), 2)
     
-    def equips_sum(self, Tables):
-        '''
-        Returns the sum of all the costs inherent to equipments in a composition.
-        '''
-        equip_sum = 0
+    # + SomaTransporte(CompCode)
+    # TODO: Find where to put the transport_sum
 
-        if not hasattr(Tables, 'equipments_table'):
-            Tables.wb_equip_import()
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
+    return sum
 
-        comp_equips = self.equipment
-        if len(comp_equips) != 0:
-            for each in comp_equips:
-                equip_code = each[0]
-                equip_qtt = each[1]
-                equip_prod = each[2]
-                equip_unprod = each[3]
-                equip_sum = equip_sum + round(
-                    equip_qtt * (equip_prod * Tables.equipments_table[equip_code][8] + equip_unprod * Tables.equipments_table[equip_code][
-                        9]), 4)
+def equips_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all the costs inherent to equipments in a composition.
+    '''
+    equip_sum = 0
 
-        return round(equip_sum, 4)
+    if not Tables.equipments.db_check(state, date, False):
+        Tables.equipments.insert_excel(state, date)
+    #TODO: Implement error handling if the equipments table doesn't exists
+
+    comp_equips = Sicro.equipments.get_equipments(comp_code)
+    if comp_equips:
+        for each in comp_equips:
+            equip_code = each[1]
+            equip_qtt = float(each[2])
+            equip_prod = float(each[3])
+            equip_unprod = float(each[4])
+            equip_sum = equip_sum + round(
+                equip_qtt * (equip_prod * float(Tables.equipments.search_state_date_code(state, date, equip_code)[0][11]) + equip_unprod * float(Tables.equipments.search_state_date_code(state, date, equip_code)[0][12])), 4)
+
+    return round(equip_sum, 4)
     
-    def labor_sum(self, Tables):
-        '''
-        Returns the sum of all the costs inherent to labors in a composition.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
-        if not hasattr(Tables, 'labor_table'):
-            Tables.wb_labor_import()
+def labor_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all the costs inherent to labors in a composition.
+    '''
+    if not Tables.labors.db_check(state, date, False):
+        Tables.labors.insert_excel(state, date)
 
-        labor_sum = 0
-        comp_labor = self.labor
-        if len(comp_labor) != 0:
-            for each in comp_labor:
-                labor_code = each[0]
-                labor_qtt = each[1]
-                labor_sum = labor_sum + round(labor_qtt * Tables.labor_table[labor_code][4], 4)
+    labor_sum = 0
+    comp_labor = Sicro.labors.get_labors(comp_code)
+    if comp_labor:
+        for each in comp_labor:
+            labor_code = each[1]
+            labor_qtt = float(each[2])
+            labor_sum = labor_sum + round(labor_qtt * float(Tables.labors.search_state_date_code(state, date, labor_code)[0][8]), 4)
 
-        return round(labor_sum, 4)
+    return round(labor_sum, 4)
 
-    def materials_sum(self, state, date, Tables):
-        '''
-        Returns the sum of all the costs inherent to materials in a composition.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
-        if not Tables.materials.db_check(state, date, False):
-            Tables.materials.insert_excel(state, date)
+def materials_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all the costs inherent to materials in a composition.
+    '''
+    if not Tables.materials.db_check(state, date, False):
+        Tables.materials.insert_excel(state, date)
 
-        material_sum = 0
-        materials = self.material
-        if len(materials) != 0:
-            for each in materials:
-                material_code = each[0]
-                material_qtt = each[1]
-                material_sum = material_sum + round(material_qtt * float(Tables.materials.search_state_date_code(state, date, material_code)[0][6]), 4)
+    material_sum = 0
+    materials = Sicro.materials.get_materials(comp_code)
+    if materials:
+        for each in materials:
+            material_code = each[1]
+            material_qtt = float(each[2])
+            material_sum = material_sum + round(material_qtt * float(Tables.materials.search_state_date_code(state, date, material_code)[0][5]), 4)
 
-        return round(material_sum, 4)
+    return round(material_sum, 4)
 
 
-    def aux_activity_sum(self, state, date, Tables):
-        '''
-        Returns the sum of all auxiliary activities costs of a composition.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
+def aux_activity_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all auxiliary activities costs of a composition.
+    '''
+    if not hasattr(Tables, 'comps'):
+        Tables.json_import()
 
-        aux_activity_sum = 0
-        aux_activity = self.aux_activity
-        if len(aux_activity) != 0:
-            for each in aux_activity:
-                aux_activity_code = each[0]
-                aux_activity_qtt = each[1]
-                aux_activity_sum = aux_activity_sum + round(aux_activity_qtt * Tables.comps[aux_activity_code].comp_sum(state, date, Tables), 4)
+    aux_activity_sum = 0
+    aux_activity = Sicro.aux_activities.get_aux_activity(comp_code)
+    if aux_activity:
+        for each in aux_activity:
+            aux_activity_code = each[1]
+            aux_activity_qtt = float(each[2])
+            aux_activity_sum = aux_activity_sum + round(aux_activity_qtt * comp_sum(aux_activity_code, state, date, Tables, Sicro), 4)
 
-        return round(aux_activity_sum, 4)
-
-
-    def fixed_time_sum(self, state, date, Tables):
-        '''
-        Returns the sum of all costs inherent to fixed time of a composition.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
-
-        fixed_time_sum = 0
-        fixed_time = self.fixed_time
-        if len(fixed_time) != 0:
-            for each in fixed_time:
-                fixed_time_code = each[1]
-                fixed_time_qtt = each[2]
-                fixed_time_sum = fixed_time_sum + round(fixed_time_qtt * Tables.comps[fixed_time_code].comp_sum(state, date, Tables), 4)
-
-        return round(fixed_time_sum, 4)
+    return round(aux_activity_sum, 4)
 
 
-    def transport_sum(self, state, date, Tables):
-        '''
-        Returns the sum of all transportation costs of a composition.
-        '''
-        if not hasattr(Tables, 'comps'):
-            Tables.json_import()
+def fixed_time_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all costs inherent to fixed time of a composition.
+    '''
 
-        transport_sum = 0
-        transport = self.transport
-        if len(transport) != 0:
-            for each in transport:
-                transport_code = each[4]
-                transport_qtt = each[1]
-                transport_sum = transport_sum + round(transport_qtt * Tables.comps[transport_code].comp_sum(state, date, Tables), 4)
+    fixed_time_sum = 0
+    fixed_time = Sicro.fixed_time.get_fixed_time(comp_code)
+    if fixed_time:
+        for each in fixed_time:
+            fixed_time_code = each[2]
+            fixed_time_qtt = float(each[3])
+            fixed_time_sum = fixed_time_sum + round(fixed_time_qtt * comp_sum(fixed_time_code, state, date, Tables, Sicro), 4)
 
-        return round(transport_sum, 4)
+    return round(fixed_time_sum, 4)
 
+
+def transport_sum(comp_code, state, date, Tables, Sicro):
+    '''
+    Returns the sum of all transportation costs of a composition.
+    '''
+
+    transport_sum = 0
+    transport = Sicro.transportation.get_transportation(comp_code)
+    if transport:
+        for each in transport:
+            transport_code = each[4]
+            transport_qtt = float(each[2])
+            transport_sum = transport_sum + round(transport_qtt * comp_sum(transport_code, state, date, Tables, Sicro), 4)
+
+    return round(transport_sum, 4)
+
+
+class Sicro:
+    def __init__(self):
+        self.general_data = db_mod.SICROGeneralData()
+        self.equipments = db_mod.SICROEquipments()
+        self.labors = db_mod.SICROLabors()
+        self.materials = db_mod.SICROMaterials()
+        self.aux_activities = db_mod.SICROAuxiliaryActivities()
+        self.fixed_time = db_mod.SICROFixedTime()
+        self.transportation = db_mod.SICROTransportation()
+    
 
 class Tables:
     def __init__(self):
         self.materials = db_mod.Materials()
+        self.equipments = db_mod.Equipments()
+        self.labors = db_mod.Labors()
 
     def json_import(self):
-
         comps = dict()
         with open('comps.json', 'r') as fp:
             dict_import = json.load(fp)
@@ -182,94 +183,8 @@ class Tables:
             comps[key] = Composition(dict_import[key][0], dict_import[key][1], dict_import[key][2], dict_import[key][3],
                                     dict_import[key][4], dict_import[key][5],
                                     dict_import[key][6], dict_import[key][7], dict_import[key][8])
-        
+
         self.comps = comps
-    
-    def wb_equip_import(self, filepath=None):
-        '''
-        Imports the equipments data from the *.xlsx file to a dictionary.
-        '''
-
-        if not filepath:
-            root = tk.Tk()
-            root.withdraw()
-            filepath = filedialog.askopenfilename(title='Selecione o arquivo referente ao Relatório Sintético de Equipamentos',
-                                                filetypes=[("Excel files", "*.xlsx")])
-
-        ps = openpyxl.load_workbook(filepath)
-        sheet = ps.active
-
-        equips = {}
-
-        row = 1
-
-        while sheet['A' + str(row)].value != "Código":
-            row += 1
-
-        row += 1
-
-        while sheet['A' + str(row)].value is None:
-            row += 1
-
-        while row <= sheet.max_row:
-            code = sheet['A' + str(row)].value
-            data1 = sheet['B' + str(row)].value
-            data2 = sheet['C' + str(row)].value
-            data3 = sheet['D' + str(row)].value
-            data4 = sheet['E' + str(row)].value
-            data5 = sheet['F' + str(row)].value
-            data6 = sheet['G' + str(row)].value
-            data7 = sheet['H' + str(row)].value
-            data8 = sheet['I' + str(row)].value
-            data9 = sheet['J' + str(row)].value
-            data10 = sheet['K' + str(row)].value
-            equips[code] = (data1, data2, data3, data4, data5, data6, data7, data8, data9, data10)
-            row = row + 1
-
-        ps.close()
-
-        self.equipments_table = equips
-
-    def wb_labor_import(self, filepath=None):
-        '''
-        Imports the labors data from the *.xlsx file to a dictionary.
-        '''
-
-        if not filepath:
-            root = tk.Tk()
-            root.withdraw()
-            filepath = filedialog.askopenfilename(title='Selecione o arquivo referente ao Relatório Sintético de Mão de Obra',
-                                                filetypes=[("Excel files", "*.xlsx")])
-
-        ps = openpyxl.load_workbook(filepath)
-        sheet = ps.active
-
-        labor = {}
-
-        row = 1
-
-        while sheet['A' + str(row)].value != "Código":
-            row += 1
-
-        row += 1
-
-        while sheet['A' + str(row)].value is None:
-            row += 1
-
-        while row <= sheet.max_row:
-            code = sheet['A' + str(row)].value
-            data1 = sheet['B' + str(row)].value
-            data2 = sheet['C' + str(row)].value
-            data3 = sheet['D' + str(row)].value
-            data4 = sheet['E' + str(row)].value
-            data5 = sheet['F' + str(row)].value
-            data6 = sheet['G' + str(row)].value
-            labor[code] = (data1, data2, data3, data4, data5, data6)
-            row += 1
-
-        ps.close()
-
-        self.labor_table = labor
 
     def wb_prices_import(self, filepath=None):
         # Imports the compositions price data from the *.xlsx file to a dictionary
@@ -303,21 +218,23 @@ class Tables:
         ps.close()
         
         self.prices = price
+    
 
 if __name__ == '__main__':
     tables = Tables()
+    sicro = Sicro()
     tables.json_import()
     comp_code = '0308265'
     state = 'PR'
-    date = '01/2021'
-    print('Equip: R$', tables.comps[comp_code].equips_sum(tables))
-    print('Labor: R$', tables.comps[comp_code].labor_sum(tables))
-    print('Material: R$', tables.comps[comp_code].materials_sum(state, date, tables))
-    print('Auxiliary activities: R$', tables.comps[comp_code].aux_activity_sum(state, date, tables))
-    print('Fixed Time: R$', tables.comps[comp_code].fixed_time_sum(state, date, tables))
-    print('Transportation: R$', tables.comps[comp_code].transport_sum(state, date, tables))
+    date = '01/2022'
+    print('Equip: R$', equips_sum(comp_code, state, date, tables, sicro))
+    print('Labor: R$', labor_sum(comp_code, state, date, tables, sicro))
+    print('Material: R$', materials_sum(comp_code, state, date, tables, sicro))
+    print('Auxiliary activities: R$', aux_activity_sum(comp_code, state, date, tables, sicro))
+    print('Fixed Time: R$', fixed_time_sum(comp_code, state, date, tables, sicro))
+    print('Transportation: R$', transport_sum(comp_code, state, date, tables, sicro))
 
-    print('Total: R$', round(tables.comps[comp_code].comp_sum(state, date, tables), 4))
+    print('Total: R$', round(comp_sum(comp_code, state, date, tables, sicro), 4))
 
 # for code in preco:
 #    if code != '0919002' and code != '0919210' and code != '7119788':
